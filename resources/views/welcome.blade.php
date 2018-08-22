8df07abe0c90c87933b08f600a59ceae43382442
Hoang Nguyen <nchoang.dn@gmail.com>
2018-08-22 14:27:06 +0700

    edit 1.3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 1.2
+                    Laravel 1.3
                 </div>
 
                 <div class="links">
